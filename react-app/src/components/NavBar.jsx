6fc9dc73907e11c815056c2ad639f62a9b0b9474
clntsf <simonfec@tcd.ie>
2025-04-10 14:45:36 +0100

    icons are in on navbar

--- react-app/src/components/NavBar.jsx
+++ react-app/src/components/NavBar.jsx
@@ -1,6 +1,7 @@
 import './styles/NavBar.css';
-import linkedIn from "../images/linkedin.svg"
-// import github from "../images/github.svg"
+import linkedIn from "../images/icons8-linkedin.svg"
+import github from "../images/icons8-github.svg"
+import email from "../images/mail.svg"
 
 export default function NavBar() {
     return (
@@ -12,10 +13,15 @@ export default function NavBar() {
                 <a className="nav-link" href="contact-page">Contact</a>
             </div>
             <div className="nav-socials">
-                <a className="navbar-logo-button" href="https://www.linkedin.com/in/colin-sf">
-                    <img src={linkedIn} className="navbar-logo" alt="linkedin" />
+                <a className="navbar-logo-button" href="mailto:colin.tsf@gmail.com">
+                    <img src={email} className="navbar-logo" alt="email" title="Email Contact"/>
                 </a>
-                {/* <button className="navbar-logo-button"><img src={github} className="navbar-logo" alt="github" /></button> */}
+                <a className="navbar-logo-button" href="https://www.linkedin.com/in/colin-sf">
+                    <img src={linkedIn} className="navbar-logo" alt="linkedin" title="LinkedIn Profile"/>
+                </a>
+                <a className="navbar-logo-button" href="https://github.com/clntsf">
+                    <img src={github} className="navbar-logo" alt="github" title="Github Profile"/>
+                </a>
             </div>
         </div>
     );
